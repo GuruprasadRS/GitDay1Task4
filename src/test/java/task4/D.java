49d6699b417b4e683569a1ce3ff0c7e0ddf5b859
GuruprasadRS <guruprasad.off.use@gmail.com>
2020-05-01 14:13:20 +0530

    4th Project

---
+++ src/test/java/task4/D.java
@@ -0,0 +1,14 @@
+package task4;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class D {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Guru\\Maven-Workspace\\GitDay1Task1\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.instagram.com/");
+		driver.manage().window().maximize();
+		driver.quit();
+	}
+}
